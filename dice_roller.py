22a2ff3bcad3ce196002d55306979004b11c49c2
dzeo <ezegwukingston@gmail.com>
2021-03-11 00:35:32 +0100

    dice function all set

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,15 @@
 import random
 
+dice_rolls=2
+dice_sum=0
+for i in range(0,dice_rolls):
+  roll =random. randint(1,6)
+  dice_sum += roll
+  print(f'You rolled a {roll}')
+print(f"you have rolled a total of {dice_sum}")
+
 def main():
   roll =random. randint(1,6)
-  print(f'You rolled a {roll}')
-  
 
 if __name__== "__main__":
   main()
